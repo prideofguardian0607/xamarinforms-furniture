62f56eaf072a9eab67b785eedfdef6827d68a927
SageDragonKing <sagedragon915@mail.ru>
2022-03-04 17:32:03 -0800

    Initial commit

---
+++ XFFurniture/XFFurniture/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Plugin.SharedTransitions;
+using Xamarin.Forms;
+
+namespace XFFurniture
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new SharedTransitionNavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
